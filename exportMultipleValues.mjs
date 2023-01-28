6c54759e9fa07523888d8e46cabb3604c021dce4
ramakrishna <krk0245@gmail.com>
2023-01-28 06:06:06 +0000

    commit-msg

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,6 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+const multiplyByFour = (n1) => {
+  return n1 * 4;
+};
+export { myArray, bulb, multiplyByFour };
